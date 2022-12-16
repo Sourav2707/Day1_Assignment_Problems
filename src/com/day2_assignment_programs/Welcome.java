c140ea2124a6dbfe823f9192463e3161189e7282
Sourav2707 <smartsourav33@gmail.com>
2022-12-16 14:58:44 +0530

    Initial commit with welcome message

---
+++ src/com/day2_assignment_programs/Welcome.java
@@ -0,0 +1,9 @@
+package com.day2_assignment_programs;
+
+import org.w3c.dom.ls.LSOutput;
+
+public class Welcome {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Day 2 assignment programs");
+    }
+}
